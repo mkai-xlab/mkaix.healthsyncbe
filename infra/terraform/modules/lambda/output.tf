07a57ee37d30600ac68b769db5fa54ef9f18d9aa
vietnd37 <vietnd37@fpt.com>
2026-06-27 16:46:48 +0700

    feat: provision AWS infrastructure, storage modules, EC2 compute setup, and CI/CD deployment pipeline

--- infra/terraform/modules/lambda/output.tf
+++
@@ -1,9 +0,0 @@
-output "lambda_arn" {
-  value       = aws_lambda_function.dicom_processor.arn
-  description = "The ARN of the DICOM processor Lambda function"
-}
-
-output "lambda_name" {
-  value       = aws_lambda_function.dicom_processor.function_name
-  description = "The name of the DICOM processor Lambda function"
-}
