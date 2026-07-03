6bf698a57a5a246b675f4c258d5f181e041ab18d
duyanhbenho <pda1232004@gmail.com>
2026-07-03 14:48:06 +0700

    feat: implement dynamic role-based access control and permission management system with initial data seeding

--- src/main/java/com/g93/be/repository/RolePermissionRepository.java
+++ src/main/java/com/g93/be/repository/RolePermissionRepository.java
@@ -10,6 +10,6 @@ public interface RolePermissionRepository extends JpaRepository<RolePermission, 
     List<RolePermission> findByRoleId(Long roleId);
     void deleteByRoleId(Long roleId);
     
-    @Query("SELECT rp.permission.name FROM RolePermission rp WHERE rp.role.name = :roleName")
-    List<String> findPermissionNamesByRoleName(@Param("roleName") String roleName);
+    @Query("SELECT rp.permission.code FROM RolePermission rp WHERE rp.role.code = :roleCode")
+    List<String> findPermissionCodesByRoleCode(@Param("roleCode") String roleCode);
 }
