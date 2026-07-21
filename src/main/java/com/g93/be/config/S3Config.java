42d86e18b977178aba43a1cb76a41fdb81869438
NguyenHoangDuy15 <nguyenhoangduy004bn@gmail.com>
2026-07-21 16:43:37 +0700

    feat: migrate environment profiles, S3 configuration exclusions, and integration test fixes

--- src/main/java/com/g93/be/config/S3Config.java
+++ src/main/java/com/g93/be/config/S3Config.java
@@ -3,10 +3,12 @@ package com.g93.be.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
+@Profile("!prod")
 public class S3Config {
 
     @Value("${app.aws.region}")
